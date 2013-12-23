87e4232b52d33fa4c15a4cf10a956a4bfc83a867
xianyu <yuxian012@gmail.com>
2013-12-23 23:31:14 +0800

    a

--- uiEditor/control/view.h
+++ uiEditor/control/view.h
@@ -16,10 +16,26 @@ protected:
     float mX;
     float mY;
     float mZ;
-    string mRelativeTo;
-    string mRelativePoint;
-    string mPoint;
+    view *mRelativeTo;
+    view *mRelativePoint;
+    view *mPoint;
     bool mHide;
+    inline float getX(){return this->mX;};
+    inline float getY(){return this->mY;};
+    inline float getZ(){return this->mZ;};
+    inline float getWidth(){return this->mWidth;};
+    inline float getHeight(){return this->mHeight;};
+    inline view  *getRelativeTo(){return this->mRelativeTo;};
+    inline view  *getRelativePoint (){return this->mRelativePoint;};
+    inline view  *getPoint(){return this->mPoint;};
+    inline bool getHide(){return this->mHide;};
+    inline void setX(float x){this->mX = x;};
+    inline void setY(float y){this->mX = y;};
+    inline void setZ(float z){this->mZ = z;};
+    inline void setWidth(float width){this->mWidth = width;};
+    inline void setHeight(float height){this->mHeight = height;};
+    inline void setRelativeTo(view *relativeTo){this->mRelativeTo = relativeTo;};
+    inline void setHide(bool isHide){this->mHide = isHide;};
 signals:
 
     
